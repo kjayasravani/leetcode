41d2ebbb655a15bbeee3ac2ca98dca09feb5b99e
K Jaya Sravani <114249522+kjayasravani@users.noreply.github.com>
2024-07-08 10:40:12 +0530

    Time: 3 ms (49.98%), Space: 41 MB (29.98%) - LeetHub

---
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int findTheWinner(int n, int k) {
+        List <Integer> friends = new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            friends.add(i);
+        }
+        int index = 0;
+        while(friends.size()>1){
+            index = (index+k-1) % friends.size();
+            friends.remove(index);
+        }
+        
+        return friends.get(0);
+    }
+}
