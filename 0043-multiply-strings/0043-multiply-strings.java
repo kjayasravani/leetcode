af500b1f936d57e21599b0492139473452d3138a
K Jaya Sravani <114249522+kjayasravani@users.noreply.github.com>
2024-07-10 11:19:41 +0530

    Time: 2 ms (97.23%), Space: 42.2 MB (60.94%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] result = new int[m + n];
+
+        // Reverse the strings to make indexing easier
+        char[] num1Chars = num1.toCharArray();
+        char[] num2Chars = num2.toCharArray();
+
+        // Perform multiplication digit by digit
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1Chars[i] - '0') * (num2Chars[j] - '0');
+                int sum = mul + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        // Convert result array to string, skipping leading zeros
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.toString();
+    }
+}
