5e1d0f5dc6d1aa1347b2509d0c2f6f5416593ca7
K Jaya Sravani <114249522+kjayasravani@users.noreply.github.com>
2024-07-13 17:21:46 +0530

    Time: 32 ms (87.5%), Space: 61 MB (56.25%) - LeetHub

--- 2846-robot-collisions/2846-robot-collisions.java
+++ 2846-robot-collisions/2846-robot-collisions.java
@@ -1,52 +1,38 @@
 class Solution {
-    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] h, String directions) {
         int n = positions.length;
-        List<int[]> robots = new ArrayList<>();
-
-        for (int i = 0; i < n; ++i) {
-            robots.add(new int[]{positions[i], healths[i], directions.charAt(i), i});
+        List<Integer> ind = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ind.add(i);
         }
+        ind.sort((a, b) -> Integer.compare(positions[a], positions[b]));
 
-        Collections.sort(robots, (a, b) -> Integer.compare(a[0], b[0]));
-
-        Stack<int[]> stack = new Stack<>();
-
-        for (int[] robot : robots) {
-            if (robot[2] == 'R' || stack.isEmpty() || stack.peek()[2] == 'L') {
-                stack.push(robot);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i : ind) {
+            if (directions.charAt(i) == 'R') {
+                stack.push(i);
                 continue;
             }
-
-            if (robot[2] == 'L') {
-                boolean add = true;
-                while (!stack.isEmpty() && stack.peek()[2] == 'R' && add) {
-                    int last_health = stack.peek()[1];
-                    if (robot[1] > last_health) {
-                        stack.pop();
-                        robot[1] -= 1;
-                    } else if (robot[1] < last_health) {
-                        stack.peek()[1] -= 1;
-                        add = false;
-                    } else {
-                        stack.pop();
-                        add = false;
-                    }
-                }
-
-                if (add) {
-                    stack.push(robot);
+            while (!stack.isEmpty() && h[i] > 0) {
+                if (h[stack.peek()] < h[i]) {
+                    h[stack.pop()] = 0;
+                    h[i] -= 1;
+                } else if (h[stack.peek()] > h[i]) {
+                    h[stack.peek()] -= 1;
+                    h[i] = 0;
+                } else {
+                    h[stack.pop()] = 0;
+                    h[i] = 0;
                 }
             }
         }
 
-        List<int[]> resultList = new ArrayList<>(stack);
-        resultList.sort(Comparator.comparingInt(a -> a[3]));
-
-        List<Integer> result = new ArrayList<>();
-        for (int[] robot : resultList) {
-            result.add(robot[1]);
+        List<Integer> res = new ArrayList<>();
+        for (int v : h) {
+            if (v > 0) {
+                res.add(v);
+            }
         }
-
-        return result;
+        return res;
     }
 }
