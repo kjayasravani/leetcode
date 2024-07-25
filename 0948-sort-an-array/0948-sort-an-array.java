2c8860b2161eb55c39f951ab69a116056152ac58
K Jaya Sravani <114249522+kjayasravani@users.noreply.github.com>
2024-07-25 12:12:06 +0530

    Time: 23 ms (75.85%), Space: 56.6 MB (32.66%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,22 +1,53 @@
-import java.util.*;
-
 class Solution {
     public int[] sortArray(int[] nums) {
-        // Convert array to ArrayList for easier manipulation
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int num : nums) {
-            arrayList.add(num);
+        int[] temp = new int[nums.length]; // Temporary array for merging
+        mergeSort(nums, temp, 0, nums.length - 1);
+        return nums;
+    }
+    
+    private void mergeSort(int[] nums, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, temp, left, mid); // Sort left half
+            mergeSort(nums, temp, mid + 1, right); // Sort right half
+            merge(nums, temp, left, mid, right); // Merge sorted halves
         }
-
-        // Sort the ArrayList
-        Collections.sort(arrayList);
-
-        // Convert ArrayList back to array
-        int[] sortedArray = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            sortedArray[i] = arrayList.get(i);
+    }
+    
+    private void merge(int[] nums, int[] temp, int left, int mid, int right) {
+        // Copy elements into temp array
+        for (int i = left; i <= right; i++) {
+            temp[i] = nums[i];
         }
-
-        return sortedArray;
+        
+        // Merge sorted halves from temp array back into nums array
+        int i = left; // Initial index for left subarray
+        int j = mid + 1; // Initial index for right subarray
+        int k = left; // Initial index for merged subarray
+        
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                nums[k] = temp[i];
+                i++;
+            } else {
+                nums[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+        
+        // Copy remaining elements of left subarray if any
+        while (i <= mid) {
+            nums[k] = temp[i];
+            i++;
+            k++;
+        }
+        
+        // Copy remaining elements of right subarray if any
+        while (j <= right) {
+            nums[k] = temp[j];
+            j++;
+            k++;
+        }
     }
 }
